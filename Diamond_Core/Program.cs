92bcd5bbbfde3b8b3a7ffd4a67a67ab39963945b
Ved <vedpatel6101@gmail.com>
2025-05-03 12:05:59 +0530

    add cors for live site

--- Diamond_Core/Program.cs
+++ Diamond_Core/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend",
         policy => policy
-            .WithOrigins("http://localhost:5173")
+            .WithOrigins("http://localhost:5173", "https://diamond-core-frontend.onrender.com")
             .AllowAnyHeader()
             .AllowAnyMethod());
 });
